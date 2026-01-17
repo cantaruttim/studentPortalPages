5faf5bafaf60b2abad62e3a61b1f2e27832261f7
Matheus Cantarutti <matheuscantarutti@Matheuss-MacBook-Air.local>
2026-01-17 14:26:29 -0300

    nav bar

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -8,15 +8,34 @@ export default function PrivateLayout({
   return (
     <>
       <nav className="bg-[#E5611C] px-8">
-        <ul className="flex h-16 items-center gap-8 text-white text-sm font-medium
-            [&_a]:cursor-pointer
-            [&_a:hover]:text-[#233059]
-            [&_a:hover]:font-bold">
-          <li><Link href="/activities">Atividades</Link></li>
-          <li><Link href="/modules">Módulos</Link></li>
-          <li><Link href="/teachers">Professores</Link></li>
-          <li><Link href="/grades">Boletim</Link></li>
-        </ul>
+        <div className="flex h-16 items-center gap-12">
+          
+          {/* LOGO */}
+          <div className="flex items-center">
+            <span className="text-xl font-bold text-[#233059]">
+              ADAI<span className="font-light italic text-white">College</span>
+            </span>
+          </div>
+
+          {/* MENU */}
+          <ul
+            className="
+              flex items-center gap-8
+              text-sm font-medium text-white
+
+              [&_a]:cursor-pointer
+              [&_a]:font-semibold
+              [&_a:hover]:text-[#233059]
+              [&_a:hover]:font-bold
+            "
+          >
+            <li><Link href="/activities">Atividades</Link></li>
+            <li><Link href="/modules">Módulos</Link></li>
+            <li><Link href="/teachers">Professores</Link></li>
+            <li><Link href="/grades">Boletim</Link></li>
+          </ul>
+
+        </div>
       </nav>
 
       <main className="px-8 py-6">
