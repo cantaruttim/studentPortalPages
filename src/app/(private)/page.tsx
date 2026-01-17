4e642798ede30c5f4b9c9fb9d5462b48b05597f2
cantaruttim <cantaruttim@outlook.com>
2026-01-17 16:19:19 -0300

    updated import

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,11 +1,15 @@
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <p className="ml-0 px-0 mt-1.5">
-        Bem vindo ao portal do aluno! <br />
-        NOME_USUARIO
+      <p 
+        className="
+          ml-0 
+          px-0 
+          mt-1.5
+        ">
+          Bem vindo ao portal do aluno! <br />
+          NOME_USUARIO
       </p>
     </div>
   );
